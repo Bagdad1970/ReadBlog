8926c0a4896c5863ebe788d35510c32ce01fe719
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-01-31 21:50:33 +0300

    Create Genre controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "genres/index"
+  resources :genres
+  resources :literary_forms
+  resources :authors
+  resources :books
+  resources :users
+  resources :user_books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
